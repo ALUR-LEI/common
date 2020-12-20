6e61309bce5716e61ba2a821d2d2b7e19e6c59e6
npullar <npullar@gmail.com>
2020-12-20 17:43:04 +1300

    Initial customization of FAO Community Server software (github.com/OpenTenure master branch most recent update at that time being December 2019)
    Allodial Land Use Registry (ALUR) software operated by the United Treaty Council and Land Equity International

---
+++ src/main/java/org/sola/common/ClaimStatusConstants.java
@@ -0,0 +1,40 @@
+package org.sola.common;
+
+public class ClaimStatusConstants {
+    
+    /** Withdrawn status of claim. */
+    public static final String WITHDRAWN = "withdrawn";
+    
+    /** Created status of claim. */
+    public static final String CREATED = "created";
+    
+    /** Rejected status of claim. */
+    public static final String REJECTED = "rejected";
+    
+    /** Reviewed status of claim. */
+    public static final String REVIEWED = "reviewed";
+    
+    /** Moderated status of claim. */
+    public static final String MODERATED = "moderated";
+    
+    /** Unmoderated status of claim. */
+    public static final String UNMODERATED = "unmoderated";
+    
+    /** Issued status of claim. */
+    public static final String ISSUED = "issued";
+    
+    /** Renewed status of claim. */
+    public static final String RENEWED = "renewed";
+    
+    /** Cancelled status FOR claim certificate. */
+    public static final String CANCELLED = "cancelled";  
+    
+    /** Historic status of claim. */
+    public static final String HISTORIC = "historic";    
+    
+    /** Approved status of boundary. */
+    public static final String APPROVED = "approved";    
+    
+    /** Revised status of boundary. */
+    public static final String REVISED = "revised";    
+}
